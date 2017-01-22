1e2e85758f49f86768f0e03d2a65474bfdbb6f17
Nellob01 <noelaniekan@gmail.com>
2017-01-22 16:30:03 -0600

    Initial commit

---
+++ src/com/company/Room.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by noelaniekan on 1/18/17.
+ */
+public class Room {
+    int row;
+    int col;
+    boolean wasVisited = false;
+    boolean hasBottom = true;
+    boolean hasRight = true;
+    boolean isStart = false;
+    boolean isEnd = false;
+
+    public Room(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+    public String toString(){
+        String coords = row + "," + col;
+        return coords;
+}}
